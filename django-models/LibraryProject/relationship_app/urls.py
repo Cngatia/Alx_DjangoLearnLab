4bf2fab3b8f7b80f201242e27754355e44fa768c
Cngatia <cngatia1@gmail.com>
2024-11-19 11:54:53 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,22 +1,48 @@
-from django.urls import path, include
-from . import views
-from django.contrib.auth.views import LoginView, LogoutView
-
+from django.urls import path
 from .views import list_books, LibraryDetailView
+urlpatterns = [
+  path('', list_books, name='index'),
+  path('detail', LibraryDetailView.as_view(), name='detail'),
+]
+
+
+
+
+
+
+from django.contrib.auth.views import LoginView,LogoutView
+from django.urls import path
+from . import views
+
+# urlpatterns = [
+#     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+#     path('logout/', LogoutView.as_view(template_name="logout.html"), name='logout'),
+#     path('register/', views.register, name='register'),
+# ]
+
+
+
+
+
+
+
+
+
+
 
 urlpatterns = [
-    path("relations/", views.bookrelation, name='bookrelation'),
-    path("add_book/", views.bookrelation, name='add_book'),
-    path("delete_book/", views.bookrelation, name='delete_book'),
-    path("edit_book/", views.bookrelation, name='edit_book'),
-    path('', views.index, name='index'),
-    path('listbooks/',views.list_books, name='listbooks'),
-    path('librarydetail/<int:pk>/', views.LibraryDetailView.as_view(), name='librarydetailview'),
-    path('register/', views.register.as_view(), name='register'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('profile/',views.ProfileView.as_view(),name='profile'),
-    path('members/', views.member_view, name='members'),
-    path('librarian/', views.librarian_view, name='librarian'),
-    path('admin/', views.admin_view, name="admin") 
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
+
+
+
+
+
+
+# urlpatterns = [
+#     path('add_book/', views.add_book, name='add_book'),
+#     path('edit_book/', views.edit_book, name='edit_book'),
+#     path('delete_book/', views.delete_book, name='delete_book'),
+# ]
