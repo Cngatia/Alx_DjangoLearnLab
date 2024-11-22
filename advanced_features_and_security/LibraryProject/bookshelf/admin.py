087cdb1bafd158739b0a64484335ec480d6dc98f
Charles Ngatia <cngatia1@gmail.com>
2024-11-22 11:13:07 +0300

    edit models

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+  list_display=["username","date_of_birth"]
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,11 +1,48 @@
+# relationship_app/models.py
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, date_of_birth, profile_photo, password=None):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth, profile_photo=profile_photo)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, date_of_birth, profile_photo, password=None):
+        user = self.create_user(username, email, date_of_birth, profile_photo, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.URLField()
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'date_of_birth', 'profile_photo']
+# bookshelf/models.py
 from django.db import models
 
-# Create your models here
-
-class book(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
-    author= models.CharField(max_length=100)
-    publication_year= models.IntegerField() 
 
-    def __str__(self) -> str:
-        return f"{self.title} by {self.author}"
+    class Meta:
+        permissions = [
+            ("can_create", 'Can create book'),
+            ("can_delete", 'Can delete book'),
+            ("can_edit", 'Can edit book'),
+            ("can_view", 'Can view book'),
+        ]
+
+    def __str__(self):
+        return self.title
