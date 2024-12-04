f0d23ed766eea065f40e0c7ff33a4325385d9661
Charles Ngatia <cngatia1@gmail.com>
2024-12-04 10:40:33 +0300

    changed underscore to hyphen in file names

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+"""
+created classes that defines the title, author and publication year models.
+"""
+    
+
+    
+    
+    
