07c67e9428605be3336abf3806e67588fbbf8ec5
Charles Ngatia <cngatia1@gmail.com>
2024-12-04 11:22:31 +0300

    commit

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Book,Author
 from datetime import date
 
-class Bookserializer(serializers.Modelserializer):
+class BookSerializer(serializers.Modelserializer):
     class Meta:
         model = Book
         fields = ("title","publication_year","author")
@@ -13,7 +13,7 @@ class Bookserializer(serializers.Modelserializer):
         return value
     
 
-class Authorserializer(serializers.Modelserializer):
+class AuthorSerializer(serializers.Modelserializer):
     book = Bookserializer(many =True, read_only = True)
     class Meta:
         model = Author
